8b506c7562d36bf2349e67df7387781894c59715
iiina <iiina@DESKTOP-A4R70HK>
2022-10-03 23:04:01 +0900

    초기 커밋.

---
+++ Practice002/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace Practice002
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            FileStream fs;
+            try
+            {
+                fs = new FileStream("Alpha.txt", FileMode.Open);
+            }
+            catch(IOException)
+            {
+                Console.WriteLine("파일을 열 수 없습니다.");
+                return;
+            }
+            StreamReader r = new StreamReader(fs);
+            string s;
+            int i = 1;
+            while((s=r.ReadLine())!=null)
+            {
+                Console.WriteLine(i + ":" + s);
+                i++;
+            }
+            r.Close();
+        }
+    }
+}
